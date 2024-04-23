75f5d0522132972c3c7d29429a28e5673b11b8d1
RutamBhagat <rutambhagat@gmail.com>
2024-04-23 17:18:05 +0530

    initial setup II

--- code_wizard/docs_to_embeddings.py
+++ code_wizard/docs_to_embeddings.py
@@ -1,5 +1,7 @@
 import os
 
+from langchain_community.document_loaders import ReadTheDocsLoader
+
 if __name__ == "__main__":
     current_dir = os.path.dirname(__file__)
     docs_path = os.path.join(
@@ -11,5 +13,5 @@ if __name__ == "__main__":
         "en",
         "latest",
     )
-
     print(docs_path)
+    # loader = ReadTheDocsLoader(docs_path)
